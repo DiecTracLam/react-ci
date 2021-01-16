04d808cb1ebb5b9e81f49a2d2c4014553790449e
DiecTracLam <diectraclam.0812@gmail.com>
2021-01-16 19:07:16 +0700

    Done

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -25,9 +25,18 @@ class Book extends Component {
         return (
             <div className="Body">
                 {this.props.books.map((book, index) => {
+                    const { volumeInfo } = book
                     return (
-                        <div className="frame_book">
-                            <img src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'http://books.google.com/books/content?id=Mdj9DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api'} alt="" />
+                        <div className='frame'>
+                            <div className="frame_book">
+                                <img src={volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : 'http://books.google.com/books/content?id=Mdj9DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api'} alt="" />
+                                <div className="frame_content">
+                                    <h1 >{volumeInfo.title}</h1>
+                                    <p><strong>Author:</strong>{volumeInfo.authors.join(', ')}</p>
+                                    <p>{volumeInfo.publisher}</p>
+                                    <a href={volumeInfo.previewLink} target="_blank">More Info</a>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
